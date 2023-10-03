8c39cfeab46f6d82aa8a56d23fa17ba45153eb39
medwards <maxedwards20@gmail.com>
2023-10-03 12:40:58 -0600

    Q2 part A puts the model into the circle

--- Q2/main.py
+++ Q2/main.py
@@ -2,81 +2,93 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ackermannCircle():
-    # TODO: Figure out how to start from the center and plot the truck path's over the circle's
-    # Conversion factor from feet to meters
-    feet_to_meters = 0.3048
-    # Constants
-    robot_length = 35.0 / feet_to_meters  # in meters
-    robot_width = 10.0 / feet_to_meters  # in meters
-    # Constants
-    circle_radius = 18.0  # in meters
-    velocity = 8.0  # in m/s
-    frequency = 2.0  # Hz
+# Discussed in Lecture on September 8 2023, September 11 2023
 
-    # Calculate the circumference of the circle
-    circle_circumference = 2 * np.pi * circle_radius
+"""Notes from Lectures:
+- September 8 2023
+    - The length of the robot matters A LOT
+    - Whe we start, what is in front of me is y ground
+    - When we turn, consider where am I in my own udnerstand and where am I in relation to the map
+    Timestamp at 35:00 - 40:00
+    - Geometry gives us L/R = tan(alpha), therefore the R = L/tan(alpha)
+    - The real world for phi dot, change in global theta, is phi dot = v_rearWheel/R
+    - Equations 1.4 a b c are all the equations we need for question 2
+    - Equations 1.11a, 1.11b, and 1.11c will help for the skid steer question (part b)
+- September 11 2023
+    - For part 3 we know the circle and the velocity of the robot, meaning at every time step we know what the ground truth is
+    - Now we simply go along the circle and calculate the error at each time step, and graph it
+    
 
-    # Calculate the time it takes to complete one circle
-    circle_time = circle_circumference / velocity
+"""
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+FEET_TO_METERS = 0.3048
 
-    # Calculate the number of points to simulate at 2Hz
-    num_points = int(circle_time * frequency)
+VEHICLE_LENGTH = 35 / FEET_TO_METERS # length of the vehicle
+VEHICLE_WIDTH = 10 / FEET_TO_METERS # width of the vehicle
+VEHICLE_VELOCITY = 8 # m/s
+UPDATE_SPEED = 0.5 # 2 hz
+def ackermanCircle():
+    ## Questions:
+    # Is alpha up to me to decide or can we try different values?
+    # How do I keep this within the radius of the circle?
+    # Will the skid steer version of this essentially be the same except for the equation modifications? Yes, and to get to the edge we can move forwd.
+    # What is the forward Euler method? 
+    x_coordinates = [0]
+    y_coordinates = [0]
+    theta_history = [0]
+    # Move robot to get to the edge of the circle
+    getToEdge(x_coordinates, y_coordinates, theta_history)
+    moveCircle(x_coordinates, y_coordinates, theta_history)
+    # TODO: Create a function to up to the top. Calculate radius of inner circle to get to the edge, then move over there before starting
+    plt.plot(x_coordinates, y_coordinates)
+    plt.show()
 
-    # Initialize arrays to store x and y coordinates
-    x_coordinates = []
-    y_coordinates = []
-    angular_velocities = []
+def getToEdge(x_coordinates, y_coordinates, theta_history):
+    innerRadius = 9
+    # alpha is the inverse tangent of the ratio of the length of the vehicle to the radius of the circle
+    alpha = np.arctan(VEHICLE_LENGTH/innerRadius) # equation 1.1
+    seconds = 2 * np.pi * innerRadius / VEHICLE_VELOCITY # 2 pi r / v
+    moveRobot(x_coordinates, y_coordinates, theta_history, seconds/2, alpha)
 
-    # Calculate the angle increment for each time step
-    angle_increment = 2 * np.pi / num_points
-
-    # Simulate the motion starting at the edge of the circle
-    for i in range(num_points):
-        angle = i * angle_increment
-        x = circle_radius * np.cos(angle)
-        y = circle_radius * np.sin(angle)
-        x_coordinates.append(x)
-        y_coordinates.append(y)
-    plot(x_coordinates, y_coordinates, angular_velocities)
+def moveCircle(x_coordinates, y_coordinates, theta_history):
+    radius = 18
+    seconds = 2 * np.pi * radius / VEHICLE_VELOCITY # 2 pi r / v
+    alpha = np.arctan(VEHICLE_LENGTH/radius) # equation 1.1
+    moveRobot(x_coordinates, y_coordinates, theta_history, seconds, alpha)
+     
+def moveRobot(x_coordinates, y_coordinates, theta_history, number_seconds, alpha):
+        for _ in range(int(number_seconds/UPDATE_SPEED)):
+            x_velocity = -VEHICLE_VELOCITY*np.sin(theta_history[-1]) # x dot
+            y_velocity = VEHICLE_VELOCITY*np.cos(theta_history[-1]) # y dot
+            omega = VEHICLE_VELOCITY/VEHICLE_LENGTH*np.tan(alpha) # angular velocity
+            x_next = x_coordinates[-1] + x_velocity*UPDATE_SPEED
+            y_next = y_coordinates[-1] + y_velocity*UPDATE_SPEED
+            theta_next = theta_history[-1] + omega*UPDATE_SPEED
+            x_coordinates.append(x_next)
+            y_coordinates.append(y_next)
+            theta_history.append(theta_next)
+        
 
 def skidSteer():
+    # Skid steer setup will just be straight line, get to edge, then rotate 90 degrees
     # TODO: Implement the same circle mechanism, but using skid steer insted of the ackerman model
     pass
 
 def positionalError():
     pass
+    circleRadius = 9 # meters
+    # We know the radius of circle and velocity of robot, meaning at every time step we know what the ground truth is:
+    # Now we simply go along the circle and calculate the error at each time step, and graph it
+    
     #TODO: Use Euler method to calculate the positional error of the robot
     # TODO: Graph the rrors for 3 time steps 1, 0.1, and 0.01
 
-def plot(x_coordinates, y_coordinates, angular_velocities):
-
-    # Plot the circle
-    circle = plt.Circle((0, 0), 18, color="black", fill=False)
-
-
-    # Plot the path
-    plt.figure(figsize=(10, 6))
-    plt.plot(x_coordinates, y_coordinates, label="Path", color="blue")
-    plt.xlabel("X (meters)")
-    plt.ylabel("Y (meters)")
-    plt.title("Robot Path along the Edge of a Circle")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-    # Plot the angular velocities
-    plt.figure(figsize=(10, 4))
-    plt.plot(angular_velocities)
-    plt.xlabel("Time Steps")
-    plt.ylabel("Angular Velocity (rad/s)")
-    plt.title("Angular Velocity over Time")
-    plt.grid(True)
-    plt.show()
-
 
 def main():
-    ackermannCircle()
+    ackermanCircle()
 
 
 if __name__ == "__main__":
