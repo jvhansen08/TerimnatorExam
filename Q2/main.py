ea60e9af71e2c74325ce9244cdf5f310d8e9d6a0
medwards <maxedwards20@gmail.com>
2023-10-03 12:57:49 -0600

    Q2 parts a and b done

--- Q2/main.py
+++ Q2/main.py
@@ -1,31 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
-
-# Discussed in Lecture on September 8 2023, September 11 2023
-
-"""Notes from Lectures:
-- September 8 2023
-    - The length of the robot matters A LOT
-    - Whe we start, what is in front of me is y ground
-    - When we turn, consider where am I in my own udnerstand and where am I in relation to the map
-    Timestamp at 35:00 - 40:00
-    - Geometry gives us L/R = tan(alpha), therefore the R = L/tan(alpha)
-    - The real world for phi dot, change in global theta, is phi dot = v_rearWheel/R
-    - Equations 1.4 a b c are all the equations we need for question 2
-    - Equations 1.11a, 1.11b, and 1.11c will help for the skid steer question (part b)
-- September 11 2023
-    - For part 3 we know the circle and the velocity of the robot, meaning at every time step we know what the ground truth is
-    - Now we simply go along the circle and calculate the error at each time step, and graph it
-    
-
-"""
-import math
-import random
 import numpy as np
-import matplotlib.pyplot as plt
 FEET_TO_METERS = 0.3048
-
 VEHICLE_LENGTH = 35 / FEET_TO_METERS # length of the vehicle
 VEHICLE_WIDTH = 10 / FEET_TO_METERS # width of the vehicle
 VEHICLE_VELOCITY = 8 # m/s
@@ -44,9 +19,8 @@ def ackermanCircle():
     moveCircle(x_coordinates, y_coordinates, theta_history)
     # TODO: Create a function to up to the top. Calculate radius of inner circle to get to the edge, then move over there before starting
     plt.plot(x_coordinates, y_coordinates)
-    plt.savefig("ackerman.png")
-    plt.close()
-    # plt.show()
+    # plt.savefig("ackerman.png")
+    plt.show()
 
 def getToEdge(x_coordinates, y_coordinates, theta_history):
     innerRadius = 9
@@ -62,6 +36,7 @@ def moveCircle(x_coordinates, y_coordinates, theta_history):
     moveRobot(x_coordinates, y_coordinates, theta_history, seconds, alpha)
      
 def moveRobot(x_coordinates, y_coordinates, theta_history, number_seconds, alpha):
+        # Equations are from Mobile-Robots
         for _ in range(int(number_seconds/UPDATE_SPEED)):
             x_velocity = -VEHICLE_VELOCITY*np.sin(theta_history[-1]) # x dot
             y_velocity = VEHICLE_VELOCITY*np.cos(theta_history[-1]) # y dot
@@ -74,8 +49,7 @@ def moveRobot(x_coordinates, y_coordinates, theta_history, number_seconds, alpha
             theta_history.append(theta_next)
         
 def skidSteer():
-    # Skid steer setup will just be straight line, get to edge, then rotate 90 degrees
-    # TODO: Implement the same circle mechanism, but using skid steer insted of the ackerman model
+    # Skid steer setup will just be move to edge, rotate 90 degrees, then move in a circle
     x_coordinates = [0]
     y_coordinates = [0]
     theta_history = [0]
@@ -88,18 +62,14 @@ def skidSteer():
     # Complete the circle
     moveCircle(x_coordinates, y_coordinates, theta_history)
     plt.plot(x_coordinates, y_coordinates)
-    plt.savefig("skidSteer.png")
-    # plt.show()
-
-    
+    # plt.savefig("skidSteer.png")
+    plt.show()
 
 def skidSteerGetToEdge(x_coordinates, y_coordinates, theta_history):
-    # Skid steer can simply move in a line up to the edge of the circle, then rotate 90 degrees
     moveRobot(x_coordinates, y_coordinates, theta_history, 2, 0)
 
 
 def positionalError():
-    pass
     circleRadius = 9 # meters
     # We know the radius of circle and velocity of robot, meaning at every time step we know what the ground truth is:
     # Now we simply go along the circle and calculate the error at each time step, and graph it
@@ -109,7 +79,21 @@ def positionalError():
 
 
 def main():
-    # ackermanCircle()
+    """Notes from Lectures:
+    - September 8 2023
+        - The length of the robot matters A LOT
+        - Whe we start, what is in front of me is y ground
+        - When we turn, consider where am I in my own udnerstand and where am I in relation to the map
+        Timestamp at 35:00 - 40:00
+        - Geometry gives us L/R = tan(alpha), therefore the R = L/tan(alpha)
+        - The real world for phi dot, change in global theta, is phi dot = v_rearWheel/R
+        - Equations 1.4 a b c are all the equations we need for question 2
+        - Equations 1.11a, 1.11b, and 1.11c will help for the skid steer question (part b)
+    - September 11 2023
+        - For part 3 we know the circle and the velocity of the robot, meaning at every time step we know what the ground truth is
+        - Now we simply go along the circle and calculate the error at each time step, and graph it
+    """
+    ackermanCircle()
     skidSteer()
 
 
